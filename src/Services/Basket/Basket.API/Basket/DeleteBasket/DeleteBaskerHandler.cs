a07d74f07fa861e0d8cd40fa4a6230bed1db6698
Dac Hieu <dachieu287@gmail.com>
2024-08-14 15:50:58 +0700

    develop delete basket feature

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBaskerHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBaskerHandler.cs
@@ -1,5 +1,21 @@
 ﻿namespace Basket.API.Basket.DeleteBasket;
 
-public class DeleteBaskerHandler
+public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+
+public record DeleteBasketResult(bool IsSuccess);
+
+public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
 {
+    public DeleteBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class DeleteBaskerCommandHandler : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        return new DeleteBasketResult(true);
+    }
 }
--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -1,5 +1,26 @@
-﻿namespace Basket.API.Basket.DeleteBasket;
+﻿using Basket.API.Basket.StoreBasket;
 
-public class DeleteBasketEndpoint
+namespace Basket.API.Basket.DeleteBasket;
+
+public record DeleteBasketResponse(bool IsSuccess);
+
+public class DeleteBasketEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/basket/{userName}", async (string userName, ISender sender) =>
+        {
+            var result = await sender.Send(new DeleteBasketCommand(userName));
+
+            var respose = result.Adapt<DeleteBasketResponse>();
+
+            return Results.Ok(respose);
+        })
+            .WithName("DeleteBasket")
+            .Produces<StoreBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Basket")
+            .WithDescription("Delete Basket");
+    }
 }
