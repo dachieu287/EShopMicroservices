5db54ca4b4a2f25178b73427bcacbad58966764d
Dac Hieu <dachieu287@gmail.com>
2024-09-25 16:43:22 +0700

    add discountdb

--- src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContext.cs
@@ -5,5 +5,5 @@ namespace Discount.Grpc.Data;
 
 public class DiscountContext(DbContextOptions<DiscountContext> options) : DbContext(options)
 {
-    public DbSet<Coupon> Coupons { get; set; }
+    public DbSet<Coupon> Coupons { get; set; } = default!;
 }
