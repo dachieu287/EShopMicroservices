d85572da24d1d81cb9230c5e5d01e682c2e4ec2e
Dac Hieu <dachieu287@gmail.com>
2024-10-24 17:30:13 +0700

    ordering - develop rich-domain model

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -1,15 +1,15 @@
 ﻿namespace Ordering.Domain.Models;
 
-public class Order : Aggregate<Guid>
+public class Order : Aggregate<OrderId>
 {
 
     private readonly List<OrderItem> _orderItems = [];
 
     public IReadOnlyList<OrderItem> OrderItems => _orderItems;
 
-    public Guid CustomerId { get; private set; } = default;
+    public CustomerId CustomerId { get; private set; } = default!;
 
-    public string OrderName { get; private set; } = default!;
+    public OrderName OrderName { get; private set; } = default!;
 
     public Address ShippingAddress { get; private set; } = default!;
 
@@ -23,5 +23,53 @@ public class Order : Aggregate<Guid>
     {
         get => OrderItems.Sum(x => x.Price * x.Quantity);
         private set { }
+    }
+
+    public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+    {
+        var order = new Order
+        {
+            Id = id,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            Status = OrderStatus.Pending,
+        };
+
+        order.AddDomainEvent(new OrderCreatedEvent(order));
+
+        return order;
+    }
+
+    public void Update(OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment, OrderStatus orderStatus)
+    {
+        OrderName = orderName;
+        ShippingAddress = shippingAddress;
+        BillingAddress = billingAddress;
+        Payment = payment;
+        Status = orderStatus;
+
+        AddDomainEvent(new OrderUpdatedEvent(this));
+    }
+
+    public void Add(ProductId productId, int quatity, decimal price)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quatity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var orderItem = new OrderItem(Id, productId, quatity, price);
+
+        _orderItems.Add(orderItem);
+    }
+
+    public void Remove(ProductId productId)
+    {
+        var orderItem = _orderItems.FirstOrDefault(x => x.ProductId == productId);
+        if (orderItem is not null)
+        {
+            _orderItems.Remove(orderItem);
+        }
     }
 }
--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -1,8 +1,23 @@
 ﻿namespace Ordering.Domain.Models;
 
-public class Product : Entity<Guid>
+public class Product : Entity<ProductId>
 {
     public string Name { get; set; } = default!;
 
-    public string Prrice { get; set; } = default!;
+    public decimal Prrice { get; set; } = default!;
+
+    public static Product Create(ProductId id, string name, decimal price)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(name);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var product = new Product
+        {
+            Id = id,
+            Name = name,
+            Prrice = price,
+        };
+
+        return product;
+    }
 }
