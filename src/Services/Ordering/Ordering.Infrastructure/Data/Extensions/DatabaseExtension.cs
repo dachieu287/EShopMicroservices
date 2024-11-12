d77b76c1efb23c69601c635467118f23ff6f5f10
Dac Hieu <dachieu287@gmail.com>
2024-11-12 15:51:26 +0700

    ordering - auto run migration when start app

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Extensions/DatabaseExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ordering.Infrastructure.Data.Extensions;
+
+public static class DatabaseExtension
+{
+    public static async Task InitialiseDatabaseAsync(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+
+        var context = scope.ServiceProvider.GetRequiredService<DbContext>();
+
+        await context.Database.MigrateAsync();
+    }
+}
